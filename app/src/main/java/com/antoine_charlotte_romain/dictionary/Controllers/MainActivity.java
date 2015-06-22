b5b83f7f1d5dd830c5761f4701bc4734f2d6a50f
SombreDiable <le-demon-fou-de-metal@hotmail.fr>
2015-06-22 11:17:51 +0100

    Creating packages Controllers and ModelData

--- app/src/main/java/com/antoine_charlotte_romain/dictionary/Controllers/MainActivity.java
+++ app/src/main/java/com/antoine_charlotte_romain/dictionary/Controllers/MainActivity.java
@@ -1,9 +1,11 @@
-package com.antoine_charlotte_romain.dictionary;
+package com.antoine_charlotte_romain.dictionary.Controllers;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.antoine_charlotte_romain.dictionary.R;
 
 
 public class MainActivity extends ActionBarActivity {
---
+++ app/src/main/java/com/antoine_charlotte_romain/dictionary/ModelData/DataBaseSchema.java
@@ -0,0 +1,7 @@
+package com.antoine_charlotte_romain.dictionary.ModelData;
+
+/**
+ * Created by summer1 on 22/06/2015.
+ */
+public class DataBaseSchema {
+}
