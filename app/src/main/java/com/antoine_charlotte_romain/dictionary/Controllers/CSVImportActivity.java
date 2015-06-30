69b21e97c5d7119983dd526b87dcf86a026bb6b9
Charlotte TOUCHARD <charlotte.touchard@insa-rouen.fr>
2015-06-30 15:23:11 +0100

    CSV import : it is possible to import a CSV file and add its words to a specified dictionary

--- app/src/main/java/com/antoine_charlotte_romain/dictionary/Controllers/CSVImportActivity.java
+++ app/src/main/java/com/antoine_charlotte_romain/dictionary/Controllers/CSVImportActivity.java
@@ -1,5 +1,7 @@
 package com.antoine_charlotte_romain.dictionary.Controllers;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
@@ -19,8 +21,10 @@ import com.antoine_charlotte_romain.dictionary.DataModel.DictionaryDataModel;
 import com.antoine_charlotte_romain.dictionary.DataModel.WordDataModel;
 import com.antoine_charlotte_romain.dictionary.R;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -54,7 +58,8 @@ public class CSVImportActivity extends AppCompatActivity {
             dictionaryName = intent.getStringExtra(EXTRA_NEW_DICO_NAME);
         }
 
-        List<File> csvDispo = this.getAvailableCSV(Environment.getExternalStorageDirectory());
+        // Get all available CSV files in the mobile
+        final List<File> csvDispo = this.getAvailableCSV(Environment.getExternalStorageDirectory());
 
         // Display results
         vue = (ListView) findViewById(R.id.resultsList);
@@ -64,16 +69,15 @@ public class CSVImportActivity extends AppCompatActivity {
         // if there is no result to display
         if(csvDispo.size()==0){
             element = new HashMap<String, String>();
-            element.put("name","No CSV found");
+            element.put("name", "No CSV found");
             liste.add(element);
         }
 
+        // Fill the list to display with the name of the CSV files found
         for(int i = 0 ; i < csvDispo.size() ; i++) {
             // we add each word of the results list in this new list
             element = new HashMap<String, String>();
-
             element.put("name", String.valueOf(csvDispo.get(i)));
-
             liste.add(element);
         }
 
@@ -86,25 +90,29 @@ public class CSVImportActivity extends AppCompatActivity {
         // Give ListView to the SimpleAdapter
         vue.setAdapter(adapter);
 
+        // Listener on the list
         vue.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
+            // When an item of the list is clicked
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                /*HashMap<String, String> item = (HashMap<String, String>) parent.getItemAtPosition(position);
-                Long idword = Long.parseLong(item.get("id"));
-                Long dicoid = Long.parseLong(item.get("dicoid"));
-                String headword = item.get("headword");
-                String translation = item.get("translation");
-                String note = item.get("note");
-
-                Intent intent = new Intent(AdvancedSearchResultActivity.this, WordActivity.class);
-                intent.putExtra("selectedWord", new Word(idword,dicoid,headword,translation,note));
-
-                DictionaryDataModel ddm = new DictionaryDataModel(getApplicationContext());
-                intent.putExtra("selectedDictionary", ddm.select(dicoid));
-
-                startActivity(intent);*/
-                HashMap<String, String> item = (HashMap<String, String>) parent.getItemAtPosition(position);
-                importCSV(item.get("name"));
+                // Import the chosen CSV file in the previously specified dictionary
+                importCSV(csvDispo.get(position));
+                // Display a pop up window
+                new AlertDialog.Builder(CSVImportActivity.this)
+                        .setTitle(R.string.csvimport_popuptitle)
+                        .setMessage(R.string.csvimport_popupmessage)
+                        .setPositiveButton(R.string.csvimport_popuppositive, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // pop up closes and the list of the words in this dictionary is displayed
+                            }
+                        })
+                        .setNegativeButton(R.string.csvimport_popupnegative, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // pop up closes and it is possible to import another CSV file
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
             }
         });
     }
@@ -112,7 +120,7 @@ public class CSVImportActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_csvimport, menu);
         return true;
     }
 
@@ -137,14 +145,20 @@ public class CSVImportActivity extends AppCompatActivity {
      * @return
      *          A list of CSV file names
      */
-    public List<File> getAvailableCSV(File f){
+    private List<File> getAvailableCSV(File f){
         List<File> csvDispo = new ArrayList<File>();
 
+        // if the specified file is a directory
         if (f.isDirectory()){
-            for (File childFile : f.listFiles()){
-                csvDispo.addAll(getAvailableCSV(childFile));
+            // if it contains other files
+            if (f.listFiles() != null) {
+                // for each file it contains, get all CSV files available in it
+                for (File childFile : f.listFiles()) {
+                    csvDispo.addAll(getAvailableCSV(childFile));
+                }
             }
         } else if (f.isFile()){
+            // Check if the file is a CSV file or not
             if (f.getName().endsWith(".csv") || f.getName().endsWith(".CSV")){
                 csvDispo.add(f);
             }
@@ -155,38 +169,44 @@ public class CSVImportActivity extends AppCompatActivity {
     /**
      * Open a CSV file and add its word to a dictionary
      *
-     * @param csvFileName
-     *          The CSV file name providing the words to add in the dictionary
+     * @param fileToRead
+     *          The CSV file providing the words to add in the dictionary
      */
-    private void importCSV(String csvFileName){
+    private void importCSV(File fileToRead){
         // Get the dictionary in which the words have to be added
         DictionaryDataModel ddm = new DictionaryDataModel(this);
         Dictionary d = ddm.select(dictionaryName); // if the dico exists !!!
         // if it's a new one, when do we create it ? NOW
         if (d == null)
             d = new Dictionary(dictionaryName);
+        Long dicoID = d.getId();
 
         WordDataModel wdm = new WordDataModel(this);
 
         BufferedReader br = null;
         String line = "";
+        // Each line of the CSV file will by split by the comma character
         String cvsSplitBy = ",";
         try {
-            InputStream is = getResources().getAssets().open(csvFileName); // TO CHANGE
-            br = new BufferedReader(new InputStreamReader(is));
+            InputStream is = new BufferedInputStream(new FileInputStream(fileToRead));
+            // ISO-8859-1 interprets accents correctly
+            br = new BufferedReader(new InputStreamReader(is,"ISO-8859-1"));
             String[] wordInfo;
             String note;
             String translation;
             while ((line = br.readLine()) != null) {
-                // use comma as separator
+                // Split the line with comma as a separator
                 wordInfo = line.split(cvsSplitBy);
                 note = "";
                 translation = "";
+                // if a translation exists
                 if (wordInfo.length >= 2)
                     translation = extractWord(wordInfo[1]);
-                if (wordInfo.length == 3)
+                // if a note exists
+                if (wordInfo.length >= 3)
                     note = extractWord(wordInfo[2]);
-                wdm.insert(new Word(d.getId(),extractWord(wordInfo[0]),translation,note));
+                // Add the word in the database
+                wdm.insert(new Word(dicoID,extractWord(wordInfo[0]),translation,note));
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -212,10 +232,19 @@ public class CSVImportActivity extends AppCompatActivity {
      */
     private String extractWord(String s){
         String word = s;
-        String splitBy = "\'";
+        String splitBy = "'"; // Use the simple quote as a separator
         String[] strings = s.split(splitBy);
-        if (strings.length == 1){
-            word = strings[0];
+        // if the array has more than 1 element, a simple quote was found
+        if (strings.length >= 2){
+            // The second element of the array is recorded
+            word = strings[1];
+
+            for (int i = 2; i<strings.length-1; i++){
+                // Being in that case means that there is a simple quote in the word itself
+                // So we concatenate all the elements of the array, avoiding the first and the last element
+                // and we had the suppressed simple quote
+                word = word + "'" + strings[i];
+            }
         }
         return word;
     }
